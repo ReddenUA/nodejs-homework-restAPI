f5ad5af20e048f7216a446fe1a91404a7f837eb8
ReddenUA <96418744+ReddenUA@users.noreply.github.com>
2023-03-11 14:51:27 +0100

    hw-06

--- models/user.js
+++ models/user.js
@@ -30,6 +30,18 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+    },
+
+
+
   },
   { versionKey: false, timestamps: true }
 );
@@ -50,12 +62,18 @@ const updateSubJoiSchema = Joi.object({
   subscription: Joi.string().valid("starter", "pro", "business").required(),
 });
 
+const verifyEmailJoiSchema = Joi.object({
+  email: Joi.string().pattern(emailRegex).required(),
+});
+
+
 const User = model("user", userSchema);
 
 const userSchemas = {
   registerJoiSchema,
   loginJoiSchema,
   updateSubJoiSchema,
+  verifyEmailJoiSchema,
 };
 
 module.exports = {
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -43,4 +43,13 @@ router.patch(
   ctrlWrapper(ctrlUser.updateAvatar)
 );
 
+router.get("/verify/:verificationToken", ctrlWrapper(ctrlUser.verifyEmail));
+
+router.post(
+  "/verify",
+  validation(userSchemas.verifyEmailJoiSchema),
+  ctrlWrapper(ctrlUser.resendVerifyEmail)
+);
+
+
 module.exports = router;
